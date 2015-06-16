209cde85c25d420f29cf58d3ed9c3f2b431b275f
hraynaud <herby.raynaud@gmail.com>
2015-06-16 14:41:29 -0400

    Fixes start_email click

--- ab_vs_em.js
+++ ab_vs_em.js
@@ -16,7 +16,7 @@ var emailId="GKQ9J8L";
     $(".js-modal-close, .modal-overlay").trigger("click");
   });
 
-  $("#start_email").on("click", function(e){
+  $("body").on("click", "#start_email", function(e){
      $("#step1").show();
      $(".header").hide();
      $(this).hide();
