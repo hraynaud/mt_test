faa037eb3f458285db81031f02193b2f5734081f
hraynaud <herby.raynaud@gmail.com>
2015-06-16 13:31:17 -0400

    Adds support for modal dialog

--- ab_vs_em.js
+++ ab_vs_em.js
@@ -8,10 +8,12 @@ var emailId="GKQ9J8L";
   var emailSurvey = '<script>(function(t,e,s,o){var n,c,r;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],r=e.createElement(s),r.type="text/javascript",r.async=!0,r.id=o,r.src=["https:"===location.protocol?"https://":"http://","widget.surveymonkey.com/collect/website/js/O6iOVOPN7x_2FwMk9IDv_2F9TkryrmnssZM0rvLCDZ05_2B7sTLxjQFiop1NLog6FlFKQs.js"].join(""),c.parentNode.insertBefore(r,c))})(window,document,"script","smcx-sdk");</script>';
 
   $(".step").hide();
-  $("#start").click(function(){
-    $("#step1").show();
-    $(".header").hide();
-    $(this).hide();
+
+  $("#start").click(function(e){
+     $("#step1").show();
+     $(".header").hide();
+     $("#begin").hide();
+    $(".js-modal-close, .modal-overlay").trigger("click");
   });
 
   $("#goto2").click(function(e){
